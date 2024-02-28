c4ed854d3680907447d16f1a52c660908620644b
MkGodh <fenryrvla@yandex.ru>
2024-02-28 15:57:00 +0300

    Добавьте файлы проекта.

---
+++ KyKyClock/KyKyClock.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int hours;
+    int count = 0;
+    cout << "Введите который сейчас час: "; cin >> hours;
+
+    while (count != hours) {
+        if (hours > 23 || hours <= 0) {
+            cout << "Не верное значение!"; break;
+        }
+        cout << "ку-ку" << "\n";
+        count++;
+    }
+
+
+
+
+
+
+}
